fd9aeabde7727561ce0d26e1f643a588609a708d
Kehinde Malagu <Kehindemalagu@gmail.com>
2023-02-05 21:56:21 +0100

    Initial Setup

---
+++ readme.md
@@ -0,0 +1,6 @@
+Vite(React) 4.1 and Tailwind 3.2.4 Template
+
+Run
+npx tailwindcss -i ./input.css -o ./src/output.css --watch
+npm run dev
+gitgnore nodemodules after cloning
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className='text-5xl'>HELLo</h1>
+
+    </div>
+      
+  )
+}
+
+export default App
